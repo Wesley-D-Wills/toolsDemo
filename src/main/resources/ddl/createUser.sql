3c73cf3f67481eaa66498354fcde8ad8f595cac0
wx_1655 <weixun1655@gmail.com>
2022-03-12 14:41:09 +0800

    init

---
+++ src/main/resources/ddl/createUser.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `user` (
+                        `id` int(32) NOT NULL AUTO_INCREMENT,
+                        `username` varchar(32) NOT NULL,
+                        `age` INT ,
+                        `email` varchar(32) DEFAULT NULL,
+                        `address` varchar(64) DEFAULT NULL,
+                        `tel_num` varchar(32) DEFAULT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
