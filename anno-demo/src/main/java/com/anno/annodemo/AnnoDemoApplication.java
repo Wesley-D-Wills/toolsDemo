0612b943d3264d6b38760982d05fcd8f8e03eedb
wx_1655 <weixun1655@gmail.com>
2022-11-28 15:09:05 +0800

    验证ConfigurationProperties不同生效条件 以及Conditional 注解的功能 TargetSource 与 @Value的搭配

--- anno-demo/src/main/java/com/anno/annodemo/AnnoDemoApplication.java
+++ anno-demo/src/main/java/com/anno/annodemo/AnnoDemoApplication.java
@@ -2,17 +2,37 @@ package com.anno.annodemo;
 
 import com.anno.annodemo.config.MyJdbc;
 import com.anno.annodemo.config.MyJdbcByConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
+@EnableConfigurationProperties(MyJdbcByConfiguration.class)
 public class AnnoDemoApplication {
 
-    public static void main(String[] args) {
-//        SpringApplication.run(AnnoDemoApplication.class, args);
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-                MyJdbc.class, MyJdbcByConfiguration.class);
+    @Autowired
+    MyJdbcByConfiguration myJdbcByConfiguration;
 
-        MyJdbcByConfiguration jdbc = context.getBean("myJdbcByConfiguration", MyJdbcByConfiguration.class);
-        System.out.println(jdbc);
+    public static void main(String[] args) {
+        SpringApplication.run(AnnoDemoApplication.class, args);
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+//                MyJdbc.class, MyJdbcByConfiguration.class);
+//
+//        MyJdbcByConfiguration jdbc = context.getBean("myJdbcByConfiguration", MyJdbcByConfiguration.class);
+//        System.out.println(jdbc);
+    }
+
+    @GetMapping("/test")
+    public String get() {
+        System.out.println(myJdbcByConfiguration);
+        return "ok";
     }
 
 }
--- anno-demo/src/main/java/com/anno/annodemo/config/MyJdbcByConfiguration.java
+++ anno-demo/src/main/java/com/anno/annodemo/config/MyJdbcByConfiguration.java
@@ -1,18 +1,22 @@
 package com.anno.annodemo.config;
 
 import lombok.Data;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 
 //@ConfigurationPropertiesScan(basePackages = "com.anno.annodemo.config")
-@EnableConfigurationProperties(MyJdbcByConfiguration.class)
+//@EnableConfigurationProperties(MyJdbcByConfiguration.class)
 //@EnableAutoConfiguration
-@ConfigurationProperties(prefix = "jdbc")
 //@Component
+@ConfigurationProperties(prefix = "jdbc")
 @Data
 public class MyJdbcByConfiguration {
     private String driverClassName;
